930483d677b95450639cecf1127078f45954cf10
Callum Brown <P1616004X@LEC-ADMIN.dmu.ac.uk>
2020-01-29 10:48:24 +0000

    2nd lab 29/01/2020 event listener and nav

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello! :)");
+alert("Hello!");
+const a = document.getElementById('hello');
+
+ a.addEventListener( "click" , greeting);
+
+ function greeting(eve) {
+      console.log("test");
+      console.log(eve);
+      a.textContent = "changed!";
+ }
